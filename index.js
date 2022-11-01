e14245773c16314aff66ff20d6780874eb1da8f1
Katie Assoian <ktassoian@gmail.com>
2022-11-01 16:27:46 -0400

    BFS

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ input: array and a value
     if value is too large, move right pointer down
   if no desired value found, return -1
 */
+
+/* BFS
+
+create a queue (can be an array) and a variable to store the values of the nodes visited
+place the root node in the queue
+loop as long as there is anything in the queue
+  dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+  if there is a left property on the node dequeued - add it to the queue
+  if there is a right property on the node dequeued - add it to the queue
+return the variable that stores the values
+*/
