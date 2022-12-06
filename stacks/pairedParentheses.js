724604c4618f2a44b064a14e042648c946f76d9b
Katie Assoian <ktassoian@gmail.com>
2022-12-06 16:06:54 -0500

    paired parentheses problem

---
+++ stacks/pairedParentheses.js
@@ -0,0 +1,22 @@
+/*
+Write a function, pairedParentheses, that takes in a string as an argument. The function should return a boolean indicating whether or not the string has well-formed parentheses.
+
+You may assume the string contains only alphabetic characters, '(', or ')'.
+*/
+
+const pairedParentheses = (str) => {
+  let count = 0;
+  for (let char of str) {
+    if (char === '(') {
+      count++;
+    } else if (char === ')') {
+      if (count === 0) return false;
+      count--;
+    }
+  }
+  return count === 0;
+};
+
+pairedParentheses('(david)((abby))'); // -> true
+
+pairedParentheses('()rose(jeff'); // -> false
